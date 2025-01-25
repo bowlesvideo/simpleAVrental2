f2ba8368b214acda189665a9540a605e0a8cc9b6
bowlesvideo <132084206+bowlesvideo@users.noreply.github.com>
2025-01-25 09:57:19 -0500

    updates

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,8 +1,22 @@
 'use client'
 
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import type { PropsWithChildren } from 'react'
+
+// Only import the languages you need
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
+import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
+import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json'
+
+// Register only the languages you need
+SyntaxHighlighter.registerLanguage('jsx', jsx)
+SyntaxHighlighter.registerLanguage('typescript', typescript)
+SyntaxHighlighter.registerLanguage('javascript', javascript)
+SyntaxHighlighter.registerLanguage('bash', bash)
+SyntaxHighlighter.registerLanguage('json', json)
 
 interface Props {
   language: string
