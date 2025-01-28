75dceba973e9f58759e0bbbccad3627413bbb869
bowlesvideo <132084206+bowlesvideo@users.noreply.github.com>
2025-01-28 16:06:16 -0500

    fix sitemap issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,8 @@
 import { MetadataRoute } from 'next'
+import { getBaseUrl } from '@/lib/constants'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+  const baseUrl = getBaseUrl()
 
   return [
     {
